7ea1eda1b845c4dfb24964777ca7aa0ba5cd414f
srujan <srujan.j14@gmail.com>
2018-02-06 00:18:07 +0000

    Customized default.rb and added motd.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,22 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
 # Install apache package
+
+if node['platform_family']== "rhel" 
+       package= "httpd"
+elsif node['platform_family']== "debian"
+       package ="apache2"
+
+end
+
+
 package 'apache2' do
-    package_name 'httpd'
+    package_name package
     action :install
 end
 
-service 'apache2' do
+service 'httpd' do
     action [:start, :enable]
 end
 
+include_recipe 'apache::websites'
